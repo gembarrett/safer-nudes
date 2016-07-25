44dcd465224f8359b648f69120d036ff045bc63c
gembarrett <gem@gembarrett.com>
2016-07-25 01:13:11 -0400

    starting to work on phone-buzzes section, styling and vibration

--- src/logic/app.js
+++ src/logic/app.js
@@ -1,4 +1,18 @@
 // initialise fullPageJS
 $(document).ready(function() {
-    $('#wrapper').fullpage();
+    $('#wrapper').fullpage({
+      onLeave: function(index, nextIndex, direction){
+        var leavingSection = $(this);
+        //after leaving section 2
+        if((index == 1 && direction =='down') || (index == 3 && direction == 'up')){
+          // check if vibrate and on small screen size
+          if((Modernizr.vibrate)&&(window.width < 500)) {
+            console.log('yay');
+            window.navigator.vibrate([200, 100, 200]);
+          } else {
+            console.log('boo');
+          }
+        }
+      }
+    });
 });
