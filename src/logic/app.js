ac73f78eacb2e6e8b7aa74f7450d76dedc03b207
gembarrett <gem@gembarrett.com>
2016-07-26 01:37:40 -0400

    phone now demands nudes

--- src/logic/app.js
+++ src/logic/app.js
@@ -5,18 +5,26 @@ $(document).ready(function() {
         var loadedSection = $(this);
         index = loadedSection.index();
         var phone = document.getElementById('phone-buzz');
+        var phone2 = document.getElementById('phone-scroll');
         if (index == 1) {
           if((Modernizr.vibrate)&&(window.width < 500)) {
             window.navigator.vibrate([200, 100, 200]);
           } else {
             phone.style.webkitAnimationPlayState = "running";
             phone.style.MozAnimationPlayState = "running";
-            console.log(phone.style.MozAnimationPlayState);
+            phone2.style.webkitAnimationPlayState = "paused";
+            phone2.style.MozAnimationPlayState = "paused";
           }
+        } else if (index == 2) {
+          phone2.style.webkitAnimationPlayState = "running";
+          phone2.style.MozAnimationPlayState = "running";
+          phone.style.webkitAnimationPlayState = "paused";
+          phone.style.MozAnimationPlayState = "paused";
         } else {
           phone.style.webkitAnimationPlayState = "paused";
           phone.style.MozAnimationPlayState = "paused";
-          console.log(phone.style.MozAnimationPlayState);
+          phone2.style.webkitAnimationPlayState = "paused";
+          phone2.style.MozAnimationPlayState = "paused";
         }
       }
     });
